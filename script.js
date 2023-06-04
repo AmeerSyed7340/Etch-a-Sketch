f1ebc2ae67bee69b96f3070fde17ec91711a34fb
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2023-06-04 17:40:04 -0400

    Added button UIs corresponding to size, paint, erase and reset

--- script.js
+++ script.js
@@ -5,39 +5,136 @@ container.style.width = '500px';
 const containerHeight = parseFloat(container.style.height);
 const containerWidth = parseFloat(container.style.width);
 
-console.log(containerHeight);
-//insert into webpage here
+//insert into webpage
 document.body.append(container);
 
-let cells = prompt(`Number of rows and columns: `);
+//Global variable - default size
+let cells = 16; 
 
-const numberOfCells = cells * cells;
-for(i = 0; i < numberOfCells; i++){
-    const grid = document.createElement('div');
-    grid.classList.add('grid');
-    grid.style.height = `${containerHeight/cells}px`;
-    grid.style.width = `${containerHeight/cells}px`;
-    container.appendChild(grid);
+function chooseSize(){
+    const size = document.querySelector('#btnSize');
+    size.addEventListener('click', (e) =>{
+        cells = prompt("Enter # of rows and columns:");
+        if(cells === null){
+            return;
+        }
+        else{
+            cells  = parseInt(cells);
+            if(cells < 1 || cells > 100){
+                alert(`Size must be between 1 and 100`);
+                return;
+            }
+            else{
+                modifyCells();
+            } 
+        }
+    });
 }
 
-const allGrid = document.querySelectorAll('.grid');
-let isDragging = false;
-allGrid.forEach(grid => {    
-    grid.addEventListener('mousedown', (e) => {
-        e.preventDefault();
-        isDragging = true;
+function modifyCells(){
+    const allGrid = document.querySelectorAll('.grid');
+    allGrid.forEach(grid => {
+        grid.remove();
     });
-    grid.addEventListener('mousemove', (e) => {
-        e.preventDefault();
-        if(isDragging){
+    generateCells();
+    sketch();
+}
+
+function generateCells(){
+    const numberOfCells = cells * cells;
+    for(i = 0; i < numberOfCells; i++){
+        const grid = document.createElement('div');
+        grid.classList.add('grid');
+        grid.style.height = `${containerHeight/cells}px`;
+        grid.style.width = `${containerHeight/cells}px`;
+        container.appendChild(grid);
+    }
+}
+
+function sketch(){
+    const allGrid = document.querySelectorAll('.grid');
+    let isDragging = false;
+    allGrid.forEach(grid => {    
+        grid.addEventListener('mousedown', (e) => {
+            e.preventDefault();
+            isDragging = true;
             e.target.classList.add('colorGrid');  
-        }                
+        });
+        grid.addEventListener('mousemove', (e) => {
+            e.preventDefault();
+            if(isDragging){
+                e.target.classList.add('colorGrid');  
+            }                
+        })
+        grid.addEventListener('mouseup', (e) => {
+            e.preventDefault();
+            isDragging = false;
+        })
     })
-    grid.addEventListener('mouseup', (e) => {
-        e.preventDefault();
-        isDragging = false;
+    document.addEventListener('mouseup', (e) => {
+            isDragging = false;
     })
-})
-document.addEventListener('mouseup', (e) => {
-        isDragging = false;
+}
+
+function removeSketch(){
+    const allGrid = document.querySelectorAll('.grid');
+    let isDragging = false;
+    allGrid.forEach(grid => {    
+        grid.addEventListener('mousedown', (e) => {
+            e.preventDefault();
+            isDragging = true;
+            e.target.classList.remove('colorGrid');
+        });
+        grid.addEventListener('mousemove', (e) => {
+            e.preventDefault();
+            if(isDragging){
+                e.target.classList.remove('colorGrid');  
+            }                
+        })
+        grid.addEventListener('mouseup', (e) => {
+            e.preventDefault();
+            isDragging = false;
+        })
     })
+    document.addEventListener('mouseup', (e) => {
+            isDragging = false;
+    })
+}
+
+function erase(){
+    const erase = document.querySelector('#btnErase');
+    erase.addEventListener('click', (e) =>{
+        removeSketch();
+    });
+}
+
+function paint(){
+    const paint = document.querySelector('#btnPaint');
+    paint.addEventListener('click', (e) =>{
+        sketch();
+    });
+}
+
+function reset(){
+    const allGrid = document.querySelectorAll('.grid');
+    allGrid.forEach(grid => {
+        grid.classList.remove('colorGrid');
+    });
+}
+
+function resetBtn(){
+    const resetButton = document.querySelector('#btnReset');
+    resetButton.addEventListener('click', (e)=>{
+        reset();
+    })
+}
+//function calls
+//Allowed to generate cells and sketch initially
+generateCells();
+sketch();
+
+chooseSize();
+paint();
+erase();
+resetBtn();
+
